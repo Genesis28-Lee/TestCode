06d2eae8afcac50b0d238650ed80c693ee10cd21
Genesis.Lee <g.genesis5068@gmail.com>
2025-08-04 11:32:06 +0900

    Create BackgroundWriterService.cs

---
+++ EF.DB/DbContext/BackgroundWriterService.cs
@@ -0,0 +1,119 @@
+using Commons.Utilities;
+
+using EF.Data.DBContexts;
+
+using System.Collections.Concurrent;
+
+namespace EF.Data.Services;
+
+public class BackgroundWriterService : IDisposable
+{
+    private abstract class WriteJobBase
+    {
+        public int RetryCount { get; set; } = 0;
+        public abstract Task ExecuteAsync(dRAST3Context context);
+    }
+
+    private class WriteJob : WriteJobBase
+    {
+        public Func<dRAST3Context, Task> Action { get; }
+        public TaskCompletionSource<bool> Tcs { get; } = new(TaskCreationOptions.RunContinuationsAsynchronously);
+
+        public WriteJob(Func<dRAST3Context, Task> action)
+        {
+            Action = action;
+        }
+
+        public override async Task ExecuteAsync(dRAST3Context context)
+        {
+            await Action(context);
+            Tcs.SetResult(true);
+        }
+    }
+
+    private class WriteJob<T> : WriteJobBase
+    {
+        public Func<dRAST3Context, Task<T>> Action { get; }
+        public TaskCompletionSource<T> Tcs { get; } = new(TaskCreationOptions.RunContinuationsAsynchronously);
+
+        public WriteJob(Func<dRAST3Context, Task<T>> action)
+        {
+            Action = action;
+        }
+
+        public override async Task ExecuteAsync(dRAST3Context context)
+        {
+            var result = await Action(context);
+            Tcs.SetResult(result);
+        }
+    }
+
+    private readonly BlockingCollection<WriteJobBase> _queue = new();
+    private readonly CancellationTokenSource _cts = new();
+    private readonly Func<dRAST3Context> _contextFactory;
+    private readonly Task _worker;
+
+    public BackgroundWriterService(Func<dRAST3Context> contextFactory)
+    {
+        _contextFactory = contextFactory;
+        _worker = Task.Run(ConsumeQueueAsync);
+    }
+
+    public Task EnqueueWriteAsync(Func<dRAST3Context, Task> action)
+    {
+        var job = new WriteJob(action);
+        _queue.Add(job);
+        return job.Tcs.Task;
+    }
+
+    public Task<T> EnqueueWriteAsync<T>(Func<dRAST3Context, Task<T>> action)
+    {
+        var job = new WriteJob<T>(action);
+        _queue.Add(job);
+        return job.Tcs.Task;
+    }
+
+    private async Task ConsumeQueueAsync()
+    {
+        foreach (var job in _queue.GetConsumingEnumerable(_cts.Token))
+        {
+            bool success = false;
+            while (!success && job.RetryCount < 3)
+            {
+                using var context = _contextFactory();
+                try
+                {
+                    await job.ExecuteAsync(context);
+                    success = true;
+                }
+                catch (Exception ex)
+                {
+                    job.RetryCount++;
+                    Logger.WriteLogAndTrace(LogTypes.Debug, $"[WriteJob Error - Retry {job.RetryCount}] {ex.Message}");
+
+                    if (job.RetryCount >= 3)
+                    {
+                        switch (job)
+                        {
+                            case WriteJob wj:
+                                wj.Tcs.TrySetException(ex);
+                                break;
+                            case WriteJob<object> wjo:
+                                wjo.Tcs.TrySetException(ex);
+                                break;
+                        }
+                    }
+
+                    await Task.Delay(300);
+                }
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        _cts.Cancel();
+        _queue.CompleteAdding();
+        _worker.Wait();
+    }
+}
